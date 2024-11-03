d657d61706a180d94a6c7bdde78182fc0493cb06
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-11-03 08:58:55 -0500

    add Navbar.css

--- fronted/src/components/Navbar/Navbar.jsx
+++ fronted/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,23 @@
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
+import { useState } from "react";
 
 const Navbar = () => {
+
+  const [menu, setMenu] = useState("home");
+  console.log(menu)
   return (
     <div className="navbar">
       <img src={assets.logo} className="logo" alt="" />
       <ul className="navbar-menu">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile App</li>
-        <li>Contact Us</li>
+        <li className={menu === "home" ? "active" : ""} onClick={() => setMenu("home")}>Home</li>
+        <li className={menu === "menu" ? "active" : ""} onClick={() => setMenu("menu")} >Menu</li>
+        <li className={menu === "mobile-app" ? "active" : ""} onClick={() => setMenu("mobile-app")}>Mobile App</li>
+        <li className={menu === "contact-us" ? "active" : ""} onClick={() => setMenu("contact-us")}>Contact Us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
-        <div className="navbar-basket-icon">
+        <div className="navbar-search-icon">
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
