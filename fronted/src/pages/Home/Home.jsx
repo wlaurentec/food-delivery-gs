04a25fc988ca88017e97c3bc4304bd3fe1d7acec
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-11-21 14:35:03 -0500

    add Food Display

--- fronted/src/pages/Home/Home.jsx
+++ fronted/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu"
 import Header from "../../components/Header/Header"
 import "./Home.css"
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay"
 
 const Home = () => {
 
@@ -11,6 +12,7 @@ const Home = () => {
     <div>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   )
 }
