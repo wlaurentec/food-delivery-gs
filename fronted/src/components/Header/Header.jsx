382c9f7d6bd879687bca8578ea70ef1b0fcc17db
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-11-21 07:20:03 -0500

    add header component

---
+++ fronted/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus impedit vero eveniet laboriosam est cum, enim ipsam dicta modi id fugiat, inventore molestiae corrupti quaerat pariatur labore. Pariatur, fugiat architecto.
+        Aut eum veritatis quibusdam fugit laborum incidunt, architecto expedita suscipit odio. </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ fronted/src/pages/Cart/Cart.jsx
@@ -0,0 +1,10 @@
+import "./Cart.css"
+const Cart = () => {
+  return (
+    <div>
+      Cart
+    </div>
+  )
+}
+
+export default Cart
