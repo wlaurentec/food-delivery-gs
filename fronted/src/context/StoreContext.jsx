aaeb83357b808e1e204d216210191b45ab170ab1
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-11-28 10:11:12 -0500

    add Cart page

--- fronted/src/context/StoreContext.jsx
+++ fronted/src/context/StoreContext.jsx
@@ -17,9 +17,21 @@ const StoreContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
 
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
+    }
+    return totalAmount;
+  }
+
   useEffect(() => {
     console.log(cartItems);
   }, [cartItems]);
+  
 
   const contextValue = {
     food_list,
@@ -27,6 +39,7 @@ const StoreContextProvider = (props) => {
     setCartItems,
     addToCart,
     removeFromCart,
+    getTotalCartAmount
   };
 
   return (
