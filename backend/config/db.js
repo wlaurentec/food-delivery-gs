088df47e76d514c9e24c2a3fbec1c8c013ceebe6
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-11-28 22:00:40 -0500

    MongoDB connected

---
+++ backend/config/db.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  await mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("DB Connected"))
+    .catch((err) => console.log(err));
+};
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+dotenv.config();
 
 
 // app config
@@ -9,6 +12,9 @@ const port = 4000;
 // middlewares
 app.use(express.json());
 app.use(cors());
+
+// db connection
+connectDB();
 
 // routes
 app.get("/", (req, res) => {
