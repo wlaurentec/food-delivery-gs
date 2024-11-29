55dba28208bd6b8e13244c67091595cf46cd9156
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-11-28 21:35:35 -0500

    add backend file

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+
+
+// app config
+const app = express();
+const port = 4000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => {
+  console.log(`Server Started on http://localhost:${port}`);
+})
